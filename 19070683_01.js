725b4ac43a8116eeafaa7b9bd30688a0384cc3eb
AzucenaMarquez <119988901+AzucenaMarquez@users.noreply.github.com>
2022-12-06 21:58:26 -0600

    Add files via upload
    Practicas JavaScript

---
+++ 19070683_01.js
@@ -0,0 +1,7 @@
+let name= '          Azucena Marquez Silva               ';
+console.log('Sin trim: ', name);
+console.log('Con trimEnd: ', name.trimEnd());
+console.log('Con trimStart: ', name.trimStart());
+
+//Azucena Marquez Silva
+//Lenguajes y Automatas II
---
+++ 19070683_Practica02.js
@@ -0,0 +1,9 @@
+let frase1="El ascensor que te lleva al exito no funciona. Girard"
+let frase2=frase1.split(",");
+let frase3=frase1.split(" ");
+
+console.log("Frase 1: ", frase1)
+console.log("Frase 2: ", frase2)
+console.log("Frase 3: ", frase3)
+
+//Azucena Marquez Silva
